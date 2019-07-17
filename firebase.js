f9aabc16638595fe640c720e75a2e524eeda1389
marcocabrerahdez <marco.cabrerahdez@gmail.com>
2019-07-17 21:15:19 +0100

    add percentage circles

--- firebase.js
+++ firebase.js
@@ -48,7 +48,61 @@ function showAverage() {
   // lo mostramos
   averageContainer.style.display = "block";
   // lo rellenamos con HTML!!!
-  averageContainer.innerHTML = `<div class="pappito"> </span>Votos -> papagayo: ${papagayoCount} , marinha: ${marinhaCount}, terrazas: ${terrazasCount}</div>`;
+  averageContainer.innerHTML = `<div class="percentageCircles">
+  <div class="svg-item">
+  <svg width="100%" height="100%" viewBox="0 0 40 40" class="donut">
+    <circle class="donut-hole" cx="20" cy="20" r="15.91549430918954" fill="#fff"></circle>
+    <circle class="donut-ring" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5"></circle>
+    <circle class="donut-segment" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke-dasharray="20 80" stroke-dashoffset="25"></circle>
+    <g class="donut-text">
+
+      <text y="50%" transform="translate(0, 2)">
+        <tspan x="50%" text-anchor="middle" class="donut-percent">${papagayoCount}</tspan>   
+      </text>
+    </g>
+  </svg>
+</div> <div class="svg-item">
+<svg width="100%" height="100%" viewBox="0 0 40 40" class="donut">
+  <circle class="donut-hole" cx="20" cy="20" r="15.91549430918954" fill="#fff"></circle>
+  <circle class="donut-ring" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5"></circle>
+  <circle class="donut-segment" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke-dasharray="20 80" stroke-dashoffset="25"></circle>
+  <g class="donut-text">
+
+    <text y="50%" transform="translate(0, 2)">
+      <tspan x="50%" text-anchor="middle" class="donut-percent">${marinhaCount}</tspan>   
+    </text>
+  </g>
+</svg>
+</div> <div class="svg-item">
+<svg width="100%" height="100%" viewBox="0 0 40 40" class="donut">
+  <circle class="donut-hole" cx="20" cy="20" r="15.91549430918954" fill="#fff"></circle>
+  <circle class="donut-ring" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5"></circle>
+  <circle class="donut-segment" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke-dasharray="20 80" stroke-dashoffset="25"></circle>
+  <g class="donut-text">
+
+    <text y="50%" transform="translate(0, 2)">
+      <tspan x="50%" text-anchor="middle" class="donut-percent">${terrazasCount}</tspan>   
+    </text>
+  </g>
+</svg>
+</div></div>
+<div class=percentageTitle>
+    <div class="myTitle">
+        <p>
+            <h1>Marinha</h1>
+        </p>
+    </div>
+    <div class="myTitle">
+        <p>
+            <h1>Papagayo</h1>
+        </p>
+    </div>
+    <div class="myTitle">
+        <p>
+            <h1>Terrazas</h1>
+        </p>
+    </div>
+</div>`;
 }
 
 function addPapagayoVote() {
