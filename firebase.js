5dfe9bcb65b903726b201f2fd857e6707bb202b9
Alber Erre <arblugo@gmail.com>
2019-07-17 20:15:36 +0100

    add firebase fuctions

--- firebase.js
+++ firebase.js
@@ -14,17 +14,75 @@ firebase.initializeApp(firebaseConfig);
 var chatChannel = "hoy-se-sale-b9e60-channel-1";
 var database = firebase.database();
 
-console.log(database);
-console.log(database.ref(chatChannel));
+var allVotes = [];
+var papagayoCount = 0;
+var marinhaCount = 0;
+var terrazasCount = 0;
 
-// database.ref(chatChannel).on("child_added", snapshot => {
-//   var data = snapshot.val();
-// });
+database.ref(chatChannel).on("child_added", snapshot => {
+  var data = snapshot.val();
+  allVotes.push(data);
+});
 
-function addMessage() {
+function calculateAverage() {
+  allVotes.forEach(voto => {
+    if (voto.lugar == "papagayo") {
+      papagayoCount = papagayoCount + 1;
+    }
+    if (voto.lugar == "marinha") {
+      marinhaCount = marinhaCount + 1;
+    }
+    if (voto.lugar == "terrazas") {
+      terrazasCount = terrazasCount + 1;
+    }
+  });
+
+  console.log("papagayo:", papagayoCount);
+  console.log("marinha:", marinhaCount);
+  console.log("terrazas:", terrazasCount);
+}
+
+function showAverage() {
+  // buscamos elemento html
+  var averageContainer = document.querySelector(".averageContainer");
+  // lo mostramos
+  averageContainer.style.display = "block";
+  // lo rellenamos con HTML!!!
+  averageContainer.innerHTML = `<div class="pappito"> </span>Votos -> papagayo: ${papagayoCount} , marinha: ${marinhaCount}, terrazas: ${terrazasCount}</div>`;
+}
+
+function addPapagayoVote() {
   var message = {
-    name: "marco",
     lugar: "papagayo"
   };
   database.ref(chatChannel).push(message);
+  popUpAndHide();
+}
+
+function addMarinhaVote() {
+  var message = {
+    lugar: "marinha"
+  };
+  database.ref(chatChannel).push(message);
+  popUpAndHide();
+}
+
+function addTerrazasVote() {
+  var message = {
+    lugar: "terrazas"
+  };
+  database.ref(chatChannel).push(message);
+  popUpAndHide();
+}
+
+function popUpAndHide() {
+  alert("Gracias por votar!");
+  // seleccionamos imagenes del html
+  var imageButtonsArray = document.querySelectorAll(".section");
+  // escondemos imagenes
+  imageButtonsArray.forEach(image => (image.style.display = "none"));
+  // calculamos votos
+  calculateAverage();
+  // mostramos votos
+  showAverage();
 }
